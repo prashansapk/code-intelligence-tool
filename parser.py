cab6b7a10e2134f769fd6d78bcc6c0cbaacc7e9b
PrashansaKushwaha <kushwahaprashansa08@gmail.com>
2026-01-18 14:51:36 +0530

    handled both local and git files

--- parser.py
+++ parser.py
@@ -1,7 +1,50 @@
+from pathlib import Path
 import aiohttp
 import asyncio
 import json
 import matplotlib.pyplot as plt
+
+def fileCounter(path):
+    if path.endswith(".py"):
+        codeCounts["pythonCount"] += 1
+    if path.endswith(".ts"):
+        codeCounts["tsCount"] += 1
+    elif path.endswith(".js"):
+        codeCounts["jsCount"] += 1
+    elif path.endswith(".html"):
+        codeCounts["htmlCount"] += 1
+
+
+def getAllLines(allCode):  
+    lines = allCode.split("\n")
+    return lines
+
+def printMethodsAndClasses(code,fileName):
+    print("_______________________________")
+    print(f"Methods and classes in {fileName}-")
+    for line in code:
+        x = line.strip()
+        if x.startswith("def "):
+            x=line.find("(")
+            # There is a corner case, if there is multiline string and If a line starts with def then it will also count.
+            y=line[4:x]
+            z=y.strip()
+            print("method: ",z)
+        elif x.startswith("async def "):
+            # There is a corner case, if there are multiline spaces between async and def, that is not handled
+            x=line.find("(")
+            y=line[10:x]
+            z=y.strip()
+            print("method: ",z)
+        elif x.startswith("Class"):
+            x=line.find("(")
+            y=line[5:x]
+            z=y.strip()
+            print("class: ",z)
+    
+    print("_______________________________")
+    print("")
+    print("")
 
 async def crawlPage(session, baseUrl, relPath, branch):
     # global pythonCount, tsCount, jsCount, htmlCount
@@ -50,8 +93,9 @@ async def crawlPage(session, baseUrl, relPath, branch):
                     print(name)
                 async with session.get(path) as response:
                     content = await response.text()
+
+                fileCounter(path)
                 if path.endswith(".py"):
-                    codeCounts["pythonCount"] += 1 
                     delimiter = "data-target=\"react-app.embeddedData\">"
                     startIndex = content.find(delimiter) + len(delimiter)
                     codeStr = content[startIndex:-1]
@@ -63,30 +107,7 @@ async def crawlPage(session, baseUrl, relPath, branch):
                         blob = payload.get("blob")
                         if blob:
                             code = blob.get("rawLines")
-                            print("_______________________________")
-                            print(f"Methods and classes in {name}-")
-                            for line in code:
-                                x = line.strip()
-                                if x.startswith("def "):
-                                    x=line.find("(")
-                                    # There is a corner case, if there is multiline string and If a line starts with def then it will also count.
-                                    y=line[4:x]
-                                    z=y.strip()
-                                    print("method: ",z)
-                                elif x.startswith("Class"):
-                                    x=line.find("(")
-                                    y=line[5:x]
-                                    z=y.strip()
-                                    print("class: ",z)
-                            print("_______________________________")
-                            print("")
-                            print("")
-                elif path.endswith(".ts"):
-                    codeCounts["tsCount"] += 1
-                elif path.endswith(".js"):
-                    codeCounts["jsCount"] += 1
-                elif path.endswith(".html"):
-                    codeCounts["htmlCount"] += 1
+                            printMethodsAndClasses(code,name)
 
 codeCounts = {
     "pythonCount":0,
@@ -94,13 +115,42 @@ codeCounts = {
     "jsCount":0,
     "htmlCount":0
 }
-async def main():
-    baseUrl = input("Enter Git url: ").strip()
+
+async def crawlGit(baseUrl):
     if(baseUrl.endswith("/")):
         baseUrl = baseUrl[0:-1]
     branch = ''
     async with aiohttp.ClientSession() as session:
         await crawlPage(session, baseUrl, "", branch)
+
+def readFiles(path):
+    try:
+        for file in path.iterdir():
+            
+            isFolder = file.is_dir()
+            # print(file.name+" is a folder "+ str(isFolder))
+            if isFolder:
+                if file.name != '.git':
+                    readFiles(Path(str(path)+"/"+file.name))
+            else:
+                print(str(file.name))
+                fileCounter(str(file.name))
+                content = file.read_text()
+                linesList = getAllLines(content)
+                printMethodsAndClasses(linesList,str(file.name))
+    except Exception:
+        print("Something went wrong while reading local files")
+                
+
+
+async def main():
+    inputPath = input("Enter your local repo path or Git url: ").strip()
+    
+    if inputPath.startswith("http") or inputPath.startswith("www"):
+        await crawlGit(inputPath)
+    else:
+        readFiles(Path(inputPath))
+
 
     print(codeCounts)
 
